39fa5a5720c78c67c62d2e713e7666049f0d5967
jimmy.lin <jimmy.lin@imbee.io>
2022-11-15 00:50:03 +0800

    feat: create twitch card

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss';
+
+const SubTitle = ({ children }) => {
+  return (
+    <div className="card-subtitle">
+      {children.split(' ').map((word, index) => (
+        <div class="card-subtitle-word" style={{ transitionDelay: `${index * 40}ms` }}>
+          {word}&nbsp;
+        </div>
+      ))}
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div className="app">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="card-content">
+          <div className="card-title">First css practice from now</div>
+          <SubTitle>To be honest, I hope I can continue on practices util I become master</SubTitle>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
